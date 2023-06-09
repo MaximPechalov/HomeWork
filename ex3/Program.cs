1829fe70a81346c33f3bef8e6d116871ad8c98bb
Maxim Pechalov <magaterkolg@gmail.com>
2023-06-09 16:53:49 +0300

    ex3

---
+++ ex3/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+Console.WriteLine ("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    Console.WriteLine("Число четное");
+}
+else
+{
+    Console.WriteLine("Число нечетное");
+}

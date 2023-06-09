a89c5101c1852799a740978a8f5b45631d724199
Maxim Pechalov <magaterkolg@gmail.com>
2023-06-09 16:35:55 +0300

    ex1

---
+++ ex1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+
+Console.WriteLine("Введите перввое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+int max = num1;
+
+if (num2 > max)
+{
+    max = num2;
+}
+
+Console.WriteLine(max);
